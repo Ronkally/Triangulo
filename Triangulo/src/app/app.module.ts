03e46679fd96686c6b70d183770f4387f6ae9dd8
DanielPantojac <71659310+DanielPantojac@users.noreply.github.com>
2020-10-02 15:52:13 -0700

    About Us

--- Triangulo/src/app/app.module.ts
+++ Triangulo/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
