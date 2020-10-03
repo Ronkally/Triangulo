cc149a016732d4da9178eee67299fa2dc5c7093f
Ronkally <48075756+Ronkally@users.noreply.github.com>
2020-10-02 19:36:15 -0700

    Validacion message

--- src/models/triangulo..ts
+++ src/models/triangulo..ts
@@ -6,7 +6,7 @@ export class triangulo{
     ){}
     ReturnTriangleType() {
         if(!this.CheckIfTriangle())
-            return "Por favor ingrese dimensiones válidas de un triángulo"
+            return "Ingrese medidas válidas de un triángulo"
         if(this.ladoA == this.ladoB && this.ladoB == this.ladoC && this.ladoC == this.ladoA)
             return "El triángulo es Equilátero"
         if(this.ladoA != this.ladoB && this.ladoB != this.ladoC && this.ladoC != this.ladoA)
